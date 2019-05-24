5451d1666cf89cd75cb8fc8368a70ade0584a67b
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-05-24 13:01:24 +0200

    new upload server

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,38 +1,72 @@
 var fs = require('fs');
 var formidable = require('formidable');
+var fileName = null;
 
-exports.upload = function(request, response) {
+exports.upload = function (request, response) {
     console.log('Start upload request service');
     var form = new formidable.IncomingForm();
-    form.parse(request, function(error, fields, files) {
-        fs.renameSync(files.upload.path, "lewaro.png");
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("received image:<br/>");
-        response.write("<img src='/show' />");
+    fs.readFile("templates/upload.html", function (err, html) {
+        response.writeHead(200, {
+            "Content-Type": "text/html; charset=utf-8"
+        });
+        response.write(html);
+
+        form.parse(request, function (error, fields, files) {
+            fileName = files.upload.name;
+            fs.renameSync(files.upload.path, fileName);
+        });
+    });
+};
+
+exports.welcome = function (request, response) {
+    console.log("Starting request services.");
+    fs.readFile("templates/start.html", function (err, data) {
+        response.writeHead(200, {
+            "Content-Type": "text/html; charset=utf-8"
+        });
+
+        if (err) {
+            response.writeHead(404);
+            response.write("File not found!");
+        } else {
+            response.write(data);
+        }
         response.end();
     });
 };
 
-exports.show = function(request, response) {
-    fs.readFile('lewaro.png', 'binary', function(error, file){
+exports.show = function (request, response) {
+    fs.readFile('fileName', 'binary', function (error, file) {
         response.writeHead(200, {
             'Content-Type': 'image/png'
         });
-        response.write(file,'binary');
+        response.write(file, 'binary');
         response.end();
     });
 }
- 
-exports.welcome = function(request, response) {
-    console.log("Starting request services.");
-    fs.readFile('templates/start.html', function(err, html) {
-        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-        response.write(html);
+
+exports.style = function (request, response) {
+    fs.readFile("templates/upload.css", function (error, file) {
+        response.writeHead(200, {
+            "Content-Type": "text/css"
+        });
+        response.write(file);
         response.end();
-    });
+    })
 };
 
-exports.error = function(request, response) {
+
+exports.select = function (request, response) {
+    fs.readFile("templates/start.css", function (error, file) {
+        response.writeHead(200, {
+            "Content-Type": "text/css"
+        });
+        response.write(file);
+        response.end();
+    })
+};
+
+exports.error = function (request, response) {
     console.log('What can I do?');
     response.write('404 :(');
     response.end();
--- modules/server.js
+++ modules/server.js
@@ -1,30 +1,33 @@
 var http = require('http');
 var colors = require('colors');
-
 var handlers = require('./handlers'); // our module
 
 function start() {
- function onRequest(request, response) {
-    console.log('Got Request.'.green);
-    console.log('Request ' + request.url + 'received.');
+    function onRequest(request, response) {
+        console.log('Got Request.'.green);
+        console.log('Request ' + request.url + 'received.');
 
-    response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-
-    switch (request.url) {
-        case '/':
-        case '/start':
-        handlers.welcome(request, response);
-        break;
-        case '/upload':
-        handlers.upload(request, response);
-        break;
-        case '/show':
-        handlers.show(request, response);
-        break;
-        default:
-        handlers.error(request, response);
+        switch (request.url) {
+            case '/':
+            case '/start':
+                handlers.welcome(request, response);
+                break;
+            case '/upload':
+                handlers.upload(request, response);
+                break;
+            case '/show':
+                handlers.show(request, response);
+                break;
+            case "/style":
+                handlers.style(request, response);
+                break;
+            case '/select':
+                handlers.select(request, response);
+                break;
+            default:
+                handlers.error(request, response);
+        }
     }
- }
 
     http.createServer(onRequest).listen(9000);
 
